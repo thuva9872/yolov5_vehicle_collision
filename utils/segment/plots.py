0631ca219c25d2f91cdc0e15196e81fb8d87f7f1
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2022-11-09 14:35:58 +0530

    Border

--- utils/segment/plots.py
+++ utils/segment/plots.py
@@ -13,6 +13,81 @@ from ..general import xywh2xyxy
 from ..plots import Annotator, colors
 
 
+def plot_masks(img, masks, colors, alpha=0.5):
+    """
+    Args:
+        img (tensor): img is in cuda, shape: [3, h, w], range: [0, 1]
+        masks (tensor): predicted masks on cuda, shape: [n, h, w]
+        colors (List[List[Int]]): colors for predicted masks, [[r, g, b] * n]
+    Return:
+        ndarray: img after draw masks, shape: [h, w, 3]
+
+    transform colors and send img_gpu to cpu for the most time.
+    """
+    img_gpu = img.clone()
+    print(masks.shape)
+    from skimage.segmentation import mark_boundaries, find_boundaries
+    for i in range(masks.shape[0]):
+        bounder = find_boundaries(masks[i,:,:].cpu().numpy(), mode='thick').astype(np.uint8)
+        bounder[0:5*(bounder.shape[0] // 20), :] = 0
+        # bounder[:, 0:5*(bounder.shape[0] // 20)] = 0
+        a180 = np.rot90(m=bounder, k=2)
+        point_1 = a180.shape[0] * a180.shape[1] - np.ndarray.argmax(a180) - 1
+        r1, c1 = point_1 // bounder.shape[1], point_1 % bounder.shape[1]
+        a90 = np.rot90(m=bounder, k=1)
+        point_2 = (np.ndarray.argmax(a90) % a90.shape[1]) * a90.shape[0] + a90.shape[0] - (
+                    np.ndarray.argmax(a90) // a90.shape[1]) - 1
+        r2, c2 = point_2 // bounder.shape[1], point_2 % bounder.shape[1]
+        from skimage.draw import line, line_aa
+        rr, cc, val = line_aa(r1, c1, r2, c2)
+        val = val > 0
+        val = val.astype(int)
+        bounder[:, :] = 0
+        bounder[rr, cc] = val
+
+        masks[i] = torch.tensor(bounder)
+    num_masks = len(masks)
+    if num_masks == 0:
+        return img.permute(1, 2, 0).contiguous().cpu().numpy() * 255
+
+    # [n, 1, 1, 3]
+    # faster this way to transform colors
+    colors = torch.tensor(colors, device=img.device).float() / 255.0
+    colors = colors[:, None, None, :]
+    # [n, h, w, 1]
+    masks = masks[:, :, :, None]
+
+    # import matplotlib.pyplot as plt
+    #
+    # from skimage import data
+    # from skimage import color
+    # from skimage import morphology
+    # from skimage import segmentation
+
+    # # SLIC result
+    # slic = segmentation.slic(img.numpy(), n_segments=200, start_label=1)
+    # cv2.imshow('window_name', img.numpy())
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # # maskSLIC result
+    # m_slic = segmentation.slic(img.numpy(), n_segments=100, mask=masks[0, :, :, 0].numpy(), start_label=1)
+
+    masks_color = masks.repeat(1, 1, 1, 3) * colors * alpha
+    inv_alph_masks = masks * (-alpha) + 1
+    masks_color_summand = masks_color[0]
+    if num_masks > 1:
+        inv_alph_cumul = inv_alph_masks[:(num_masks - 1)].cumprod(dim=0)
+        masks_color_cumul = masks_color[1:] * inv_alph_cumul
+        masks_color_summand += masks_color_cumul.sum(dim=0)
+
+    # print(inv_alph_masks.prod(dim=0).shape) # [h, w, 1]
+    img_gpu = img_gpu.flip(dims=[0])  # filp channel for opencv
+    img_gpu = img_gpu.permute(1, 2, 0).contiguous()
+    # [h, w, 3]
+    img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+    return (img_gpu * 255).byte().cpu().numpy()
+
+
 @threaded
 def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg', names=None):
     # Plot image grid with labels
@@ -99,9 +174,9 @@ def plot_images_and_masks(images, targets, masks, paths=None, fname='images.jpg'
                         if mh != h or mw != w:
                             mask = image_masks[j].astype(np.uint8)
                             mask = cv2.resize(mask, (w, h))
-                            mask = mask.astype(bool)
+                            mask = mask.astype(np.bool)
                         else:
-                            mask = image_masks[j].astype(bool)
+                            mask = image_masks[j].astype(np.bool)
                         with contextlib.suppress(Exception):
                             im[y:y + h, x:x + w, :][mask] = im[y:y + h, x:x + w, :][mask] * 0.4 + np.array(color) * 0.6
                 annotator.fromarray(im)
@@ -118,8 +193,9 @@ def plot_results_with_masks(file="path/to/results.csv", dir="", best=True):
     for f in files:
         try:
             data = pd.read_csv(f)
-            index = np.argmax(0.9 * data.values[:, 8] + 0.1 * data.values[:, 7] + 0.9 * data.values[:, 12] +
-                              0.1 * data.values[:, 11])
+            index = np.argmax(
+                0.9 * data.values[:, 8] + 0.1 * data.values[:, 7] + 0.9 * data.values[:, 12] +
+                0.1 * data.values[:, 11],)
             s = [x.strip() for x in data.columns]
             x = data.values[:, 0]
             for i, j in enumerate([1, 2, 3, 4, 5, 6, 9, 10, 13, 14, 15, 16, 7, 8, 11, 12]):
